d7a4404b1b1569bb19f8366520cad20b7524cade
hannannexus <hannannexus@gmail.com>
2021-09-24 02:30:12 -0700

    search by category

--- search.php
+++ search.php
@@ -21,8 +21,40 @@
                     <a href="<?php the_permalink() ?>">
                     <h2><?php the_title(); ?></h2>
                     </a>
-                    <p><?php the_excerpt(); ?></p>
-                    <p><?php the_post_thumbnail( 'large' ); ?></p>
+                    <p><?php the_post_thumbnail( 'medium' ); ?></p>
+                    <?php 
+                        global $post;
+
+                       
+                        $product = wc_get_product( $post->ID );
+                       
+
+                        //print_r($terms);
+
+                        // $product->get_regular_price();
+                        // $product->get_sale_price();
+                        // $product->get_price();
+
+                        //category
+
+                        $terms = get_the_terms($post->ID, 'product_cat');
+                        
+
+                       
+                    ?>
+
+                    <p>Price:<?php echo $product->get_price(); ?></p>
+                    <?php 
+                          foreach ($terms as $term) {
+
+                            $product_cat = $term->name;
+                            ?>
+                              <p>Category: <?php  echo $product_cat;?></p>
+                            <?php
+                                break;
+                            }
+                    ?>
+                    
 
                 <?php endwhile; ?>
 
